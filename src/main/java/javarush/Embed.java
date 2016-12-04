08e837aa8d73ed6f6e7931b99863d3a56b4208de
Zhenya Brik <Zhenya Brik>
2016-12-04 16:24:01 +0200

    hh

--- src/main/java/javarush/Embed.java
+++ src/main/java/javarush/Embed.java
@@ -3,11 +3,12 @@ package javarush;
 /**
  * Created by User on 04.12.2016.
  */
-public class Embed {
-    String estiva = "London";
-    double x = 2.11;
-
-    public String getEstiva() {
-        return estiva;
+public class Embed
+{
+    public static void main(String[] args)
+    {
+        String date = "JULY 5 1987";
+        System.out.print(date);
     }
 }
+
---
+++ src/main/java/javarush/nero.java
@@ -0,0 +1,12 @@
+package javarush;
+
+/**
+ * Created by User on 04.12.2016.
+ */
+public class nero {
+
+    public static void main(String [] args){
+        String estiva = "current";
+        System.out.print(estiva);
+    }
+}
