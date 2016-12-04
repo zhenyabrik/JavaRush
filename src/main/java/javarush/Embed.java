ead63e10463fe1e87302c619493de2b20e706aa0
Zhenya Brik <Zhenya Brik>
2016-12-04 15:00:58 +0200

    h

---
+++ src/main/java/javarush/Embed.java
@@ -0,0 +1,13 @@
+package javarush;
+
+/**
+ * Created by User on 04.12.2016.
+ */
+public class Embed {
+    String estiva = "London";
+    double x = 2.11;
+
+    public String getEstiva() {
+        return estiva;
+    }
+}
