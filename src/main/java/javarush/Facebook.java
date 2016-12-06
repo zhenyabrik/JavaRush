67ad143dd8c8abc65e5bd513f36974b736f8c69a
zhenya <zhenyabrik2014@gmail.com>
2016-12-06 19:07:43 +0300

    g

--- src/main/java/javarush/Facebook.java
+++ src/main/java/javarush/Facebook.java
@@ -5,28 +5,29 @@ package javarush;
  */
 
 //Метод должен выводить полное ФИО студента на экран. Слова разделить пробелом.
-    public class Facebook {
+public class Facebook {
 
-        public static void main(String[] args)
-        {
-            Student student = new Student();      //создание обьекта student в класе Student
-            student.firstName = "Mark";           //присвоение переменной fisrtName обьекта student значения Mark
-            student.middleName = "Elliot";
-            student.lastName = "Zuckerberg";
-            printFullName(student);               //ссылка на метод printFullName ?
-        }
-        //метод printFullName
-        public static void printFullName(Student student)
-        {
-            System.out.println(student.firstName);    //вывод значения переменной fisrtName обьекта student  ?
-            System.out.println(student.middleName);
-            System.out.println(student.lastName);
-        }
-        //создание класса Student
-        public static class Student
-        {
-            public String firstName;   //обьявление переменной firstName типа String без значения
-            public String middleName;
-            public String lastName;
-        }
+    public static void main(String[] args)
+    {
+        Student student = new Student();      //создание обьекта student в типа объекта Student
+        student.firstName = "Mark";           //присвоение переменной fisrtName обьекта student значения Mark
+        student.middleName = "Elliot";
+        student.lastName = "Zuckerberg";
+        printFullName(student);               //вызов метода printFullName и предаем парамер student
     }
+
+    //метод printFullName
+    public static void printFullName(Student student)
+    {
+        System.out.println(student.firstName);    //вывод значения переменной fisrtName обьекта student  ?
+        System.out.println(student.middleName);
+        System.out.println(student.lastName);
+    }
+    //создание класса Student
+    public static class Student
+    {
+        public String firstName;   //обьявление переменной firstName типа String без значения
+        public String middleName;
+        public String lastName;
+    }
+}
---
+++ src/main/java/javarush/a.java
@@ -0,0 +1,8 @@
+package javarush;
+
+/**
+ * Created by Yevhenii.Bryk on 06.12.2016.
+ */
+public class a {
+
+}
